621ab1ed07a6376e91698f5239048038dc697588
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-03-24 17:29:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,7 @@ function createFareMultiplier(multiplier) {
 const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, function_of_choice) {
+  return drivers.function_of_choice;
+}
